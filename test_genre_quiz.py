63c8b08d26de07161e86b782556bebe5e35e5dd9
Eleonor <eleonortiamartin@gmail.com>
2024-04-10 09:42:03 -0400

    message

--- test_genre_quiz.py
+++ test_genre_quiz.py
@@ -5,3 +5,7 @@ def test_genre_quiz():
     quiz = GenreQuiz(0,0,0,0)
     quiz.add('pop')
     assert quiz.sort() == 'pop'
+    
+def test_genre_quiz_equality():
+    quiz = GenreQuiz(1,1,1,0)
+    assert quiz.sort() == 'electropop'
