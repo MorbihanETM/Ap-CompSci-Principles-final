ba78095efae15fe89c8d832d07089a132f488e90
Eleonor <eleonortiamartin@gmail.com>
2024-06-12 18:41:09 -0400

    update

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, redirect, render_template
 from formula_one_quiz import FormulaOne
 
+# Initiate web app with Flask
 app = Flask(__name__)
 
+# Define the starting values as 0
 driver_values = FormulaOne(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
+# Create web location that returns the cyrrent driver values
 @app.route('/values')
 def values():
     return list(driver_values.driver.items())
 
+# Create web location that resets all driver values to 0
 @app.route('/reset')
 def reset():
     driver_values.clear()
@@ -16,6 +20,7 @@ def reset():
 
 @app.route('/question/1', methods = ['GET', 'POST'])
 def first_question():
+# How are you feeling right now?
     answers = ['angry', 'sad', 'happy', 'contemplative']
 
     if request.method == 'GET':
@@ -51,6 +56,7 @@ def first_question():
 
 @app.route('/question/2', methods = ['GET', 'POST'])
 def second_question():
+# Where do you feel most comfortable?
     answers = ['on the dance floor', 'at a coffee shop', 'playing video games',
                 'at the gym']
 
@@ -88,6 +94,7 @@ def second_question():
 
 @app.route('/question/3', methods = ['GET', 'POST'])
 def third_question():
+# Which color do you prefer?
     answers = ['dark blue', 'bright red', 'papaya orange', 'emerald green']
 
     if request.method == 'GET':
@@ -124,6 +131,7 @@ def third_question():
 
 @app.route('/question/4', methods = ['GET', 'POST'])
 def fourth_question():
+# What do you prefer?
     answers = ['relishing in the misery of your enemies', 'getting a coffee with a good book', 'making abstract art with loud music in your ears', 'taking a nap']
 
     if request.method == 'GET':
@@ -161,6 +169,7 @@ def fourth_question():
 
 @app.route('/question/5', methods = ['GET', 'POST'])
 def fifth_question():
+# which profession do you prefer?
     answers = ['celebrity', 'architect', 'gamer', 'golfer']
 
     if request.method == 'GET':
@@ -234,8 +243,8 @@ def sixth_question():
 
 @app.route('/question/7', methods = ['GET', 'POST'])
 def seventh_question():
-#    
-    answers = ['', '', '', '']
+# WHat is your spirit animal?
+    answers = ['Golden Retriever', 'Spider', 'Black Cat', 'Goldfish']
 
     if request.method == 'GET':
         return render_template('question_7.html', answers = answers)
@@ -243,35 +252,36 @@ def seventh_question():
     if request.method == 'POST':
         selected = request.form['selected']
         if selected == answers[0]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Charles Leclerc')
+            driver_values.add('Lando Norris')
+            driver_values.add('Logan Sargeant')
+            driver_values.add('Daniel Ricciardo')
+            driver_values.add('Alexander Albon')
         if selected == answers[1]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Carlos Sainz')
+            driver_values.add('Fernando Alonso')
+            driver_values.add('Esteban Ocon')
+            driver_values.add('Oscar Piastri')
+            driver_values.add('Sergio Perez')
         if selected == answers[2]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Lewis Hamilton')
+            driver_values.add('Max Verstappen')
+            driver_values.add('Kevin Magnussen')
+            driver_values.add('Nico Hulkenberg')
+            driver_values.add('George Russell')
         if selected == answers[3]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Yuki Tsunoda')
+            driver_values.add('Valterri Bottas')
+            driver_values.add('Pierre Gasly')
+            driver_values.add('Zhou Guanyu')
+            driver_values.add('Lance Stroll')
     
     return redirect('/question/8')
     
 @app.route('/question/8', methods = ['GET', 'POST'])
 def eighth_question():
-    answers = []
+# Whats the ideal home?
+    answers = ['a cottage in the middle of nowhere','a new york city condo overlooking central park','a victorian style chateau','a sail boat',]
 
     if request.method == 'GET':
         return render_template('question_8.html', answers = answers)
@@ -279,108 +289,35 @@ def eighth_question():
     if request.method == 'POST':
         selected = request.form['selected']
         if selected == answers[0]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Fernando Alonso')
+            driver_values.add('Max Vertsappen')
+            driver_values.add('Valterri Bottas')
+            driver_values.add('Charles Leclerc')
+            driver_values.add('Nico Hulkenberg')
         if selected == answers[1]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Lewis Hamilton')
+            driver_values.add('Daniel Ricciardo')
+            driver_values.add('Carlos Sainz')
+            driver_values.add('Lance Stroll')
+            driver_values.add('Logan Sargeant')
         if selected == answers[2]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
+            driver_values.add('Esteban Ocon')
+            driver_values.add('Kevin Magnussen')
+            driver_values.add('Oscar Piastri')
+            driver_values.add('Sergio Perez')
+            driver_values.add('George Russell')
         if selected == answers[3]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-    
-    return redirect('/question/9')
+            driver_values.add('Zhou Guanyu')
+            driver_values.add('Yuki Tsunoda')
+            driver_values.add('Pierre Gasly')
+            driver_values.add('Lando Norris')
+            driver_values.add('Alexander Albon')
 
-@app.route('/question/9', methods = ['GET', 'POST'])
-def ninth_question():
-    answers = []
-
-    if request.method == 'GET':
-        return render_template('question_9.html', answers = answers)
-
-    if request.method == 'POST':
-        selected = request.form['selected']
-        if selected == answers[0]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[1]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[2]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[3]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-    
-    return redirect('/question/10')
-
-@app.route('/question/10', methods = ['GET', 'POST'])
-def tenth_question():
-    answers = []
-
-    if request.method == 'GET':
-        return render_template('question_10.html', answers = answers)
-
-    if request.method == 'POST':
-        selected = request.form['selected']
-        if selected == answers[0]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[1]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[2]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-        if selected == answers[3]:
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-            driver_values.add('')
-    
-    return redirect('/question/')
     return redirect('/genre')
 
 @app.route('/genre')
 def get_house():
-    return 'congratulations, the formula 1 driver that most fits you right now is ' + driver_values.sort() + '!'
+    return 'congratulations, the formula 1 driver that most fits you is ' + driver_values.sort() + '!'
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8081)
